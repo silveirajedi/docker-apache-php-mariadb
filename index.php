4bd5ccd8ba86a44d98070b15123a8aee8d9fe229
Leandro Silveira <silveira.jedi@gmail.com>
2023-01-25 10:13:00 -0300

    Teste Data

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 
+$data = new DateTime();
+
 var_dump([
     "Teste Xdebug",
     "Nome" => "Leandro",
-    "E-mail" => "silveira.jedi@gmail.com"
+    "E-mail" => "silveira.jedi@gmail.com",
+    $data->format("d/m/Y - h:i")
 ]);
 
 phpinfo();
